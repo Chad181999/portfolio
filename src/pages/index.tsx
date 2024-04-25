752d33508ee7e3ae0a9a4965ff78ce105fb075b9
Chad181999 <richardchad1899@gmail.com>
2024-04-25 09:19:44 +0800

    testing 101

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import { Inter } from '@next/font/google'
-// import Navigation from '@/components/NavBar'
-import Banner from '@/components/Home'
-import IconList from '@/components/Icon'
-import About from '@/components/About'
-import Project from '@/components/Project'
-import Contact from '@/components/Contact'
-import Footer from '@/components/Footer'
-import Seo from '@/components/Seo'
+import Navigation from '../components/NavBar'
+import Banner from '../components/Home'
+import IconList from '../components/Icon'
+import About from '../components/About'
+import Project from '../components/Project'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
+import Seo from '../components/Seo'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <>
       <Seo/>
-      {/* <Navigation/> */}
+      <Navigation/>
       <Banner/>
       <IconList/>
       <About/>
