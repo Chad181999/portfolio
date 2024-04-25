e94ef53df57b14beaa8a49aed65b03baa710382a
Chad181999 <richardchad1899@gmail.com>
2024-04-25 10:11:12 +0800

    Fixing Config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import { Inter } from '@next/font/google'
-import NavBar from '../components/NavBar'
-import Banner from '../components/Home'
-import IconList from '../components/Icon'
-import About from '../components/About'
-import Project from '../components/Project'
-import Contact from '../components/Contact'
-import Footer from '../components/Footer'
-import Seo from '../components/Seo'
+import NavBar from '@/components/NavBar'
+import Banner from '@/components/Home'
+import IconList from '@/components/Icon'
+import About from '@/components/About'
+import Project from '@/components/Project'
+import Contact from '@/components/Contact'
+import Footer from '@/components/Footer'
+import Seo from '@/components/Seo'
 
 const inter = Inter({ subsets: ['latin'] })
 
